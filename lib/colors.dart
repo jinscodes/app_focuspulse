2d18abe98cf2c97d088329f61a04fa8a67592efc
{jinsung} <jayhan0215@gmail.com>
2025-04-11 22:53:04 +0900

    JAY-FEDEV: Music player
    - add music player
    - Error: First music didn't be played.

--- lib/colors.dart
+++ lib/colors.dart
@@ -4,4 +4,5 @@ class AppColors {
   static const Color bgBeige = Color(0xFFFBF2E5);
   static const Color fontbrown = Color(0xFF875324);
   static const Color borderbrown = Color(0xFFEBC7A4);
+  static const Color playerbrown = Color(0xFFD19D71);
 }
--- lib/components/audio_player_box.dart
+++ lib/components/audio_player_box.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:focuspulse/colors.dart';
 import 'package:just_audio/just_audio.dart';
 
 class AudioPlayerBox extends StatefulWidget {
@@ -9,6 +11,7 @@ class AudioPlayerBox extends StatefulWidget {
 }
 
 class _AudioPlayerBoxState extends State<AudioPlayerBox> {
+  int _audioIndex = 1;
   late AudioPlayer _audioPlayer;
 
   @override
@@ -25,7 +28,8 @@ class _AudioPlayerBoxState extends State<AudioPlayerBox> {
 
   Future<void> _playAudio() async {
     try {
-      await _audioPlayer.setAsset('assets/sounds/wind.mp3');
+      print('assets/sounds/music$_audioIndex.mp3');
+      await _audioPlayer.setAsset('assets/sounds/music$_audioIndex.mp3');
       await _audioPlayer.play();
     } catch (e) {
       print("Error playing audio: $e");
@@ -36,26 +40,87 @@ class _AudioPlayerBoxState extends State<AudioPlayerBox> {
     await _audioPlayer.pause();
   }
 
-  Future<void> _stopAudio() async {
-    await _audioPlayer.stop();
+  void _nextAudioIndex(int index) {
+    setState(() {
+      if (_audioIndex == 5) {
+        _audioIndex = 1;
+      } else {
+        _audioIndex++;
+      }
+    });
+    _pauseAudio();
+  }
+
+  void _prevAudioIndex(int index) {
+    setState(() {
+      if (_audioIndex == 1) {
+        _audioIndex = 5;
+      } else {
+        _audioIndex--;
+      }
+    });
+    _pauseAudio();
   }
 
   @override
   Widget build(BuildContext context) {
     return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        ElevatedButton(
-          onPressed: _playAudio,
-          child: const Text("Play"),
+        Stack(
+          alignment: Alignment.center,
+          children: [
+            Image.asset(
+              "assets/images/player.png",
+              width: 180.w,
+              height: 180.h,
+              opacity: const AlwaysStoppedAnimation(0.7),
+            ),
+            StreamBuilder(
+              stream: _audioPlayer.playerStateStream,
+              builder: (context, snapshot) {
+                final playerState = snapshot.data;
+                final isPlaying = playerState?.playing ?? false;
+                return IconButton(
+                  onPressed: isPlaying ? _pauseAudio : _playAudio,
+                  icon: Icon(
+                    size: 50.sp,
+                    color: AppColors.playerbrown,
+                    isPlaying ? Icons.pause_circle : Icons.play_circle,
+                  ),
+                );
+              },
+            ),
+          ],
         ),
-        ElevatedButton(
-          onPressed: _pauseAudio,
-          child: const Text("Pause"),
-        ),
-        ElevatedButton(
-          onPressed: _stopAudio,
-          child: const Text("Stop"),
+        SizedBox(height: 10.h),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            IconButton(
+              onPressed: () => _prevAudioIndex(_audioIndex),
+              icon: Icon(
+                Icons.skip_previous_rounded,
+                size: 30.sp,
+                color: AppColors.playerbrown,
+              ),
+            ),
+            Text(
+              "Music $_audioIndex",
+              style: TextStyle(
+                fontFamily: 'howdy_duck',
+                fontSize: 20.sp,
+                color: AppColors.fontbrown,
+              ),
+            ),
+            IconButton(
+              onPressed: () => _nextAudioIndex(_audioIndex),
+              icon: Icon(
+                Icons.skip_next_rounded,
+                size: 30.sp,
+                color: AppColors.playerbrown,
+              ),
+            ),
+          ],
         ),
       ],
     );
