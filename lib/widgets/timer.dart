a71dc6a93d1ac30715c97fb67c56272aff61a31c
{jinsung} <jayhan0215@gmail.com>
2025-04-19 22:26:26 +0900

    JAY's FEDEV: publishing & getting data
    - changing the publishing
    - getting data(audioname) from provider

--- lib/widgets/timer.dart
+++ lib/widgets/timer.dart
@@ -3,21 +3,41 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:focuspulse/colors.dart';
+import 'package:focuspulse/providers/audio_provider.dart';
+import 'package:focuspulse/providers/time_provider.dart';
 
-class TimerScreen extends ConsumerWidget {
+class TimerScreen extends ConsumerStatefulWidget {
   const TimerScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<TimerScreen> createState() => _TimerScreenState();
+}
+
+class _TimerScreenState extends ConsumerState<TimerScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final noise = ref.watch(audioNameProvider);
+    final session = ref.watch(sessionProvider);
+
+    String formatTime(int minutes) {
+      final int seconds = minutes * 60;
+      final int displayMinutes = seconds ~/ 60;
+      final int displaySeconds = seconds % 60;
+      return '${displayMinutes.toString().padLeft(2, '0')}:${displaySeconds.toString().padLeft(2, '0')}';
+    }
+
     return Scaffold(
       backgroundColor: AppColors.bgTimer,
       body: Stack(
         alignment: Alignment.center,
         children: [
-          Icon(
-            Icons.play_circle,
-            size: 100.w,
-            color: AppColors.timerbrown,
+          IconButton(
+            onPressed: () => {print("you pressed the button")},
+            icon: Icon(
+              Icons.play_circle,
+              size: 100.w,
+              color: AppColors.timerbrown,
+            ),
           ),
           Center(
             child: Column(
@@ -36,7 +56,7 @@ class TimerScreen extends ConsumerWidget {
                         ),
                       ),
                       Text(
-                        '24:00',
+                        formatTime(session),
                         style: TextStyle(
                           fontFamily: 'howdy_duck',
                           fontSize: 64.sp,
@@ -59,40 +79,53 @@ class TimerScreen extends ConsumerWidget {
                   child: Padding(
                     padding: EdgeInsets.symmetric(vertical: 18.h),
                     child: Center(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
                         children: [
-                          Column(
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
                             children: [
+                              SvgPicture.asset(
+                                'assets/svg/music.svg',
+                                width: 20.w,
+                                height: 20.h,
+                              ),
+                              SizedBox(width: 10.w),
                               Text(
-                                'Noise',
+                                noise,
                                 style: TextStyle(
                                   fontFamily: 'howdy_duck',
-                                  fontSize: 20.sp,
+                                  fontSize: 14.sp,
                                   color: AppColors.bgTimer,
                                 ),
+                                textAlign: TextAlign.center,
+                                overflow: TextOverflow.ellipsis,
                               ),
+                              SizedBox(width: 10.w),
+                              Image.asset(
+                                'assets/gifs/playing.gif',
+                                width: 20.w,
+                                height: 20.h,
+                                fit: BoxFit.cover,
+                              )
                             ],
                           ),
                           ClipRRect(
                             borderRadius: BorderRadius.circular(2),
                             child: Container(
-                              width: 4.w,
-                              // height: 110.h,
+                              height: 2.w,
                               color: Colors.white,
                             ),
                           ),
-                          Column(
-                            children: [
-                              Text(
-                                'Noise',
-                                style: TextStyle(
-                                  fontFamily: 'howdy_duck',
-                                  fontSize: 20.sp,
-                                  color: AppColors.bgTimer,
-                                ),
-                              ),
-                            ],
+                          Text(
+                            "Next step",
+                            style: TextStyle(
+                              fontFamily: 'howdy_duck',
+                              fontSize: 14.sp,
+                              color: AppColors.bgTimer,
+                            ),
+                            textAlign: TextAlign.center,
+                            overflow: TextOverflow.ellipsis,
                           ),
                         ],
                       ),
