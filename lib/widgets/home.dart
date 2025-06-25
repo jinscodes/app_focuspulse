51ea87a54afe8b823a07247aa54032138e083919
{jinsung} <jayhan0215@gmail.com>
2025-06-25 17:15:50 +0900

    JAY'S FEDEV: Reform the homescreen

--- lib/widgets/home.dart
+++ lib/widgets/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:focuspulse/widgets/timer_list.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:focuspulse/models/load_timer_setting.dart';
 
 class HomeScreen extends ConsumerStatefulWidget {
   const HomeScreen({super.key});
@@ -12,8 +13,114 @@ class HomeScreen extends ConsumerStatefulWidget {
 class _HomeScreenState extends ConsumerState<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: TimerList(),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'FocusPulse',
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.settings_outlined,
+              size: 24.w,
+            ),
+            onPressed: () {
+              print('Click icon settings');
+            },
+          )
+        ],
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 20.h),
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                "Welcome to Test Timer!",
+                style: TextStyle(
+                  fontSize: 24.sp,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'space_grotesk',
+                ),
+              ),
+              SizedBox(height: 16.h),
+              Text(
+                "Select your test and sound to start the timer.",
+                style: TextStyle(
+                  fontSize: 16.sp,
+                  fontWeight: FontWeight.normal,
+                  fontFamily: 'space_grotesk',
+                ),
+              ),
+              SizedBox(height: 32.h),
+              Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Test List",
+                        style: TextStyle(
+                          fontSize: 18.sp,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          print("Click icon more");
+                        },
+                        icon: Icon(
+                          Icons.more_horiz,
+                          size: 24.w,
+                        ),
+                      ),
+                    ],
+                  ),
+                  FutureBuilder(
+                    future: loadTimerSetting(),
+                    builder: (context, snapshot) {
+                      if (!snapshot.hasData) {
+                        return const CircularProgressIndicator();
+                      }
+                      final timerSettings = snapshot.data!;
+                      return Column(
+                        children: List.generate(4, (index) {
+                          final key = timerSettings[index]['key'];
+                          return ListTile(
+                            contentPadding: EdgeInsets.zero,
+                            minTileHeight: 56.h,
+                            title: Text(
+                              key,
+                              style: TextStyle(
+                                fontSize: 16.sp,
+                                fontWeight: FontWeight.normal,
+                              ),
+                            ),
+                            trailing: Icon(
+                              Icons.chevron_right,
+                              size: 24.w,
+                              color: Colors.grey,
+                            ),
+                            onTap: () {
+                              print("Click on $key");
+                            },
+                          );
+                        }),
+                      );
+                    },
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+      // body: TimerList(),
     );
   }
 }
