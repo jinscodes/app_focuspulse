9c6b89589b11776339331d29bcaae4a56b429a64
{jinsung} <jayhan0215@gmail.com>
2025-07-16 00:24:45 +0900

    Add interstitialAd

--- lib/models/adManager.dart
+++ lib/models/adManager.dart
@@ -5,7 +5,53 @@ import 'package:google_mobile_ads/google_mobile_ads.dart';
 class AdManager {
   static final AdManager instance = AdManager();
   BannerAd? _bannerAd;
-  RewardedAd? _rewardedAd;
+  InterstitialAd? _interstitialAd;
+
+  void loadInterstitialAd({
+    required Function(InterstitialAd ad) onLoaded,
+    required Function(LoadAdError error) onFailed,
+  }) {
+    InterstitialAd.load(
+      adUnitId: Platform.isAndroid
+          ? 'ca-app-pub-4654745491099162/4918863100'
+          : 'ca-app-pub-4654745491099162/5519544626',
+      // adUnitId: Platform.isAndroid // test ad unit ID
+      //     ? 'ca-app-pub-3940256099942544/1033173712'
+      //     : 'ca-app-pub-3940256099942544/4411468910',
+      request: const AdRequest(),
+      adLoadCallback: InterstitialAdLoadCallback(
+        onAdLoaded: (ad) {
+          _interstitialAd = ad;
+          onLoaded(ad);
+        },
+        onAdFailedToLoad: (error) {
+          _interstitialAd = null;
+          onFailed(error);
+        },
+      ),
+    );
+  }
+
+  void showInterstitialAd({
+    required Function() onAdDismissed,
+    required Function(AdError error) onAdFailedToShow,
+  }) {
+    if (_interstitialAd != null) {
+      _interstitialAd!.fullScreenContentCallback = FullScreenContentCallback(
+        onAdDismissedFullScreenContent: (ad) {
+          ad.dispose();
+          _interstitialAd = null;
+          onAdDismissed();
+        },
+        onAdFailedToShowFullScreenContent: (ad, error) {
+          ad.dispose();
+          _interstitialAd = null;
+          onAdFailedToShow(error);
+        },
+      );
+      _interstitialAd!.show();
+    }
+  }
 
   void loadBannerAd() {
     _bannerAd = BannerAd(
@@ -18,6 +64,11 @@ class AdManager {
     );
 
     _bannerAd!.load();
+  }
+
+  void dispose() {
+    _bannerAd?.dispose();
+    _interstitialAd?.dispose();
   }
 
   BannerAd? get bannerAd => _bannerAd;
